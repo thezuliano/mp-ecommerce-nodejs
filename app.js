de05d1de6fa6014770871646cb6cf5b11b43fa4e
Jonnathan Wilson <jonnathanwilson@gmail.com>
2020-10-21 19:49:14 -0300

    App start Fix (para heroku)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
- 
+var exphbs = require('express-handlebars');
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -15,7 +15,15 @@ app.get('/detail', function (req, res) {
 });
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
- 
-app.listen(3000);
+
+if (process.env.PORT) {
+    console.log('Usando puerto del proceso...');
+} else {
+    console.log('Usando valor directo...');
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Escuchando en el puerto: ${port} ...`));
